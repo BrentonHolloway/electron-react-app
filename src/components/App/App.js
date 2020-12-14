579480567545a7f976cefe8b2d04b7236b0ddaa1
Brenton Holloway <brenton.holloway@gmail.com>
2020-12-14 11:14:19 +1100

    changed App.js to functional component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,59 +1,33 @@
-import { Component } from 'react'
+import { useEffect, useState } from 'react'
 import logo from '../../assets/logo.svg';
 import './App.css';
 import { channels } from '../../shared/constants';
 const { ipcRenderer } = window;
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      appName: '',
-      appVersion: '',
-    };
-  }
+function App() {
+  const [appName, setAppName] = useState(null);
+  const [appVersion, setAppVersion] = useState(null);
 
-  componentDidMount() {
+  useEffect(() => {
     ipcRenderer.send(channels.APP_INFO);
     ipcRenderer.on(channels.APP_INFO, (event, arg) => {
-      ipcRenderer.removeAllListeners(channels.APP_INFO);
-      const { appName, appVersion } = arg;
-      this.setState({ appName, appVersion });
+      setAppName(arg.appName);
+      setAppVersion(arg.appVersion);
     })
-  }
 
-  render(){
-    const { appName, appVersion } = this.state;
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} alt="logo" className="App-logo"/>
-          <p>AppName: {appName} <br/> Version: {appVersion}</p>
-        </header>
-      </div>
-    );
-  }
+    return () => {
+      ipcRenderer.removeAllListeners(channels.APP_INFO);
+    }
+  }, [])
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>AppName: {appName} <br/> Version: {appVersion}</p>
+      </header>
+    </div>
+  );
 }
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Try Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
 
 export default App;
